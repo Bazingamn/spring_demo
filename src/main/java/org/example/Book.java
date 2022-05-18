be7227f75e593814d865b075f635a3947e2d21a5
Bazingamn <1804319795@qq.com>
2022-05-18 09:56:15 +0800

    test property

---
+++ src/main/java/org/example/Book.java
@@ -0,0 +1,24 @@
+package org.example;
+
+public class Book {
+
+    private String bname;
+
+    private String bauthor;
+
+    public void setBname(String bname) {
+        this.bname = bname;
+    }
+
+    public void setBauthor(String bauthor) {
+        this.bauthor = bauthor;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bname='" + bname + '\'' +
+                ", bauthor='" + bauthor + '\'' +
+                '}';
+    }
+}
--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -3,6 +3,8 @@ package org.example;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Unit test for simple App.
@@ -16,5 +18,23 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    @Test
+    public void testAdd(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
+
+        User user = context.getBean("user", User.class);
+
+        user.add();
+    }
+
+    @Test
+    public void testBook(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("bean1.xml");
+
+        Book book = context.getBean("book", Book.class);
+
+        System.out.println(book.toString());
     }
 }
