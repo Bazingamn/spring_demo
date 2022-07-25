dcafb8382d6fc7773383858993614ab9c3e77202
Bazingamn <1804319795@qq.com>
2022-07-25 12:46:05 +0800

    test collection

--- src/main/java/org/example/testTransaction/service/accountService.java
+++ src/main/java/org/example/testTransaction/service/accountService.java
@@ -3,8 +3,12 @@ package org.example.testTransaction.service;
 import org.example.testTransaction.dao.accountDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.REPEATABLE_READ)
 public class accountService {
 
     @Autowired
@@ -12,6 +16,7 @@ public class accountService {
 
     public void trade() {
         accountDao.addMoney();
+//        int i = 10/0;
         accountDao.reduceMoney();
     }
 }
---
+++ src/test/java/org/example/testLog.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class testLog {
+
+    private static final Logger logger = LoggerFactory.getLogger(testLog.class);
+
+    public static void main(String[] args) {
+        logger.info("helllolllll");
+    }
+}
