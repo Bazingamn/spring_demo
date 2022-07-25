d44384d8782776a6804189630e1cdc5b2b395fb3
Bruce Wang <1804319795@qq.com>
2022-07-26 06:58:04 +0800

    bean lifecycle

--- src/main/java/org/example/testTransaction/service/accountService.java
+++ src/main/java/org/example/testTransaction/service/accountService.java
@@ -3,8 +3,12 @@ package org.example.testTransaction.service;
 import org.example.testTransaction.dao.accountDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(propagation = Propagation.REQUIRED, isolation = Isolation.REPEATABLE_READ)
 public class accountService {
 
     @Autowired
