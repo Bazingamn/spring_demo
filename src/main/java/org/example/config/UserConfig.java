94821cd10b68920cb4ecec364384223fd3bc1a24
Bruce Wang <1804319795@qq.com>
2022-07-27 20:13:45 +0800

    bean lifecycle

---
+++ src/main/java/org/example/config/UserConfig.java
@@ -0,0 +1,12 @@
+package org.example.config;
+
+import org.example.User;
+import org.example.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class UserConfig {
+
+
+}
